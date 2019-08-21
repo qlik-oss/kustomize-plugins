ac1497089b2ac51d76d9b05a2a6fbea5e02602dd
bearium <jacob_martin@rogers.com>
2019-08-21 10:09:50 -0400

    fixed bug

--- kustomize/plugin/qlik.com/v1/helmchart/HelmChart.go
+++ kustomize/plugin/qlik.com/v1/helmchart/HelmChart.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
@@ -99,6 +100,10 @@ func (p *plugin) Generate() (resmap.ResMap, error) {
 			return nil, err
 		}
 	}
+	err = os.RemoveAll(p.ChartHome + "/requirements")
+	if err != nil {
+		return nil, err
+	}
 
 	templatedYaml, err := p.templateHelm()
 	if err != nil {
@@ -131,16 +136,34 @@ func (p *plugin) initHelm() error {
 }
 
 func (p *plugin) fetchHelm() error {
+
 	// build helm flags
 	home := fmt.Sprintf("--home=%s", p.HelmHome)
 	untarDir := fmt.Sprintf("--untardir=%s", p.ChartHome)
-	repo := fmt.Sprintf("--repo=%s", p.ChartName)
+	repo := fmt.Sprintf("--repo=%s", p.ChartRepo)
 	helmCmd := exec.Command("helm", "fetch", home, "--untar", untarDir, repo, p.ChartVersionExp, p.ChartName)
 
 	var out bytes.Buffer
 	helmCmd.Stdout = &out
 	err := helmCmd.Run()
+	if err != nil {
+		return err
+	}
 
+	fileLocation := fmt.Sprintf("%s/%s", p.ChartHome, p.ChartName)
+	tempFileLocation := fileLocation + "-temp"
+
+	err = os.Rename(fileLocation, tempFileLocation)
+	if err != nil {
+		return err
+	}
+
+	err = copyDir(fileLocation+"-temp", p.ChartHome)
+	if err != nil {
+		return err
+	}
+
+	err = os.RemoveAll(tempFileLocation)
 	if err != nil {
 		return err
 	}
@@ -316,4 +339,68 @@ func mergeValues(root interface{}, copy interface{}) (interface{}, error) {
 		return nil, err
 	}
 	return mergedData["root"], nil
+}
+
+// copy source file to destination location
+func copyFile(source string, dest string) error {
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, sourceinfo.Mode())
+			return err
+		}
+	}
+	return nil
+}
+
+//copy source directory to destination
+func copyDir(source string, dest string) error {
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err
+	}
+	sourceDirectory, _ := os.Open(source)
+	// read everything within source directory
+	objects, _ := sourceDirectory.Readdir(-1)
+
+	// go through all files/directories
+	for _, obj := range objects {
+
+		sourceFileName := source + "/" + obj.Name()
+
+		destinationFileName := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			err := copyDir(sourceFileName, destinationFileName)
+			if err != nil {
+				return err
+			}
+		} else {
+			err := copyFile(sourceFileName, destinationFileName)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+	return nil
 }
