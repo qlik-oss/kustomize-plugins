01f1a569c5beb0ee58195a2318f6b6261a6983ea
bearium <jacob_martin@rogers.com>
2019-08-29 14:49:28 -0400

    remove move

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,3 @@ DEST_DIR=$HOME/.config/kustomize
 
 cd $KUSTOMIZE_DIR
 find . -iname '*.go' -exec sh -c 'f="{}"; GO111MODULE=on go build -buildmode plugin -o "${f/.go/.so}" $f' \;
-if [ ! -z "${XDG_CONFIG_HOME}" ]; then
-DEST_DIR=$XDG_CONFIG_HOME
-fi
-find . -name '*.so' | cpio -pdm $DEST_DIR
