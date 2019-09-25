1a961c668f51ab8b4967da8000f6759b0f32947c
Boris Kuschel <boris.kuschel@qlik.com>
2019-09-25 07:43:18 -0400

    posix shell
    Signed-off-by: Boris Kuschel <boris.kuschel@qlik.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 PROJECT_DIR="$( pwd )"
 KUSTOMIZE_DIR="$(pwd)/kustomize"
 DEST_DIR=$HOME/.config/kustomize
 
 cd $KUSTOMIZE_DIR
-find . -iname '*.go' -exec sh -c 'f="{}"; GO111MODULE=on go build -buildmode plugin -o "${f/.go/.so}" $f' \;
+find . -iname '*.go' -exec sh -c 'f="{}"; GO111MODULE=on go build -buildmode plugin -o "echo $f | sed \"s/.go/.so/\"" $f' \;
