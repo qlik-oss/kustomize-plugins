68d64add04add839f2c502f57fe38f79006ccba0
Boris Kuschel <boris.kuschel@qlik.com>
2019-09-25 08:32:57 -0400

    Fix bash script
    Signed-off-by: Boris Kuschel <boris.kuschel@qlik.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ KUSTOMIZE_DIR="$(pwd)/kustomize"
 DEST_DIR=$HOME/.config/kustomize
 
 cd $KUSTOMIZE_DIR
-find . -iname '*.go' -exec sh -c 'f="{}"; GO111MODULE=on go build -buildmode plugin -o "echo $f | sed \"s/.go/.so/\"" $f' \;
+find . -iname '*.go' -exec sh -c 'f="{}"; GO111MODULE=on go build -buildmode plugin -o $(echo $f | sed "s/\.go/.so/") $f' \;
